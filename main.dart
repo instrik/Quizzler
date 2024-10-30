0d4abd879f43ff3a1c3c818c08c79d84225e8e64
Aakash Agarwal <aakash@instrik.com>
2024-10-31 00:24:05 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:quizzler/Question.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'quiz_brain.dart';
-// trail comment 2
+// trail comment 3
 QuizBrain quizBrain = QuizBrain();
 
 void main() {
